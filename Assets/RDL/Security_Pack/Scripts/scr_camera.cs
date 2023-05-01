c3b7d9b00d1d43a38b8bf88a9e4dbfbb3050c1aa
PixLSteam <PixLSteam@users.noreply.github.com>
2023-05-01 14:23:36 +0200

    CCTV Camera

---
+++ Assets/RDL/Security_Pack/Scripts/scr_camera.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class scr_camera : MonoBehaviour
+{
+
+	public float rotate_amount;
+	public float rotate_speed;
+
+	private float initial_y;
+
+
+	// Use this for initialization
+	void Start()
+	{
+		initial_y = transform.eulerAngles.y;
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		//transform.rotation = Quaternion.Euler(transform.eulerAngles.x, (Mathf.Sin(Time.realtimeSinceStartup * rotate_speed) * rotate_amount) + transform.eulerAngles.y, transform.eulerAngles.z);
+		transform.rotation = Quaternion.Euler(transform.eulerAngles.x, initial_y + Mathf.Sin(Time.realtimeSinceStartup * rotate_speed) * rotate_amount, transform.eulerAngles.z);
+	}
+}
