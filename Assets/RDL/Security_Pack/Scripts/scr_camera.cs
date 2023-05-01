339744d98da1661d670d9d328d2cffd0efc12c49
PixLSteam <PixLSteam@users.noreply.github.com>
2023-05-01 16:05:22 +0200

    Test: switch to cctv cam after 15s

--- Assets/RDL/Security_Pack/Scripts/scr_camera.cs
+++ Assets/RDL/Security_Pack/Scripts/scr_camera.cs
@@ -6,8 +6,10 @@ public class scr_camera : MonoBehaviour
 
 	public float rotate_amount;
 	public float rotate_speed;
+	public Camera cam;
 
 	private float initial_y;
+	private bool rendered = false;
 
 
 	// Use this for initialization
@@ -21,5 +23,15 @@ public class scr_camera : MonoBehaviour
 	{
 		//transform.rotation = Quaternion.Euler(transform.eulerAngles.x, (Mathf.Sin(Time.realtimeSinceStartup * rotate_speed) * rotate_amount) + transform.eulerAngles.y, transform.eulerAngles.z);
 		transform.rotation = Quaternion.Euler(transform.eulerAngles.x, initial_y + Mathf.Sin(Time.realtimeSinceStartup * rotate_speed) * rotate_amount, transform.eulerAngles.z);
+
+		if (Time.realtimeSinceStartup >= 15 && !rendered)
+        {
+			rendered = true;
+			//cam.Render();
+			Camera mainCam = Camera.main;
+			cam.tag = "MainCamera";
+			cam.targetTexture = null;
+			mainCam.enabled = false;
+        }
 	}
 }
