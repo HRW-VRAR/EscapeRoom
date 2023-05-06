3b04e0bb5eb5f329e4604a5bb74be9e6ec27f239
PixLSteam <PixLSteam@users.noreply.github.com>
2023-05-06 14:29:56 +0200

    Keypad script

--- Assets/Keypad.cs
+++ Assets/Keypad.cs
@@ -1,9 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class Keypad : MonoBehaviour
 {
+    private static readonly int MAX_DIGITS = 4;
+
+    public TMPro.TextMeshPro display;
+    public string code;
+
+    public UnityEvent correctCodeEntered;
+
+    private string text = string.Empty;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +24,35 @@ public class Keypad : MonoBehaviour
     void Update()
     {
         
+    }
+
+    private void UpdateDisplay()
+    {
+        display.text = text;
+    }
+
+    public void AppendString(string c)
+    {
+        if (text.Length + c.Length > MAX_DIGITS)
+        {
+            return;
+        }
+
+        text += c;
+        UpdateDisplay();
+    }
+
+    public void ClearText()
+    {
+        text = string.Empty;
+        UpdateDisplay();
+    }
+
+    public void Enter()
+    {
+        if (text == code)
+        {
+            correctCodeEntered.Invoke();
+        }
     }
 }
